c458e4314bc909710e9fbf5c6e5ac0dffbeb7fd9
aschwalbc <alvaro.schwalb@lshtm.ac.uk>
2024-01-23 14:53:56 +0000

    Output processing and result extraction

--- scripts/06_results.R
+++ scripts/06_results.R
@@ -36,44 +36,30 @@ outs <- rbindlist(lapply(df_names, function(df_name) {
 
 rm(list = setdiff(ls(), "outs"))
 
-outs_m <- outs %>% 
-  arrange(type, run, time) %>% 
-  mutate(cPCF = cPCFs + cPCFr, # Cost of PCF (DSTB + DRTB)
-         cRxPCF = cRxPCFs + cRxPCFr, # Cost of treatment PCF (DSTB + DRTB)
-         cRxTP = cRxTPs + cRxTPr, # Cost of treatment ACF TP (DSTB + DRTB)
-         cRxFP = cRxFPs + cRxFPr) %>% # Cost of treatment ACF FP (DSTB + DRTB)
-  mutate(cCF = cPCF + cACF,
-         cRx = cRxPCF + cRxTP + cRxFP) %>% 
-  group_by(run, time) %>% 
-  mutate(prTBc = tTBc/tTBc[type == 'base'], # Proportional TB prevalence to BAU
-         dfTBc = abs(tTBc - tTBc[type == 'base']), # TB prevalence difference to BAU
-         prMor = rMor/rMor[type == 'base'], # Proportional TB mortality to BAU
-         dfMor = abs(tMor - tMor[type == 'base'])) %>%  # TB mortality difference to BAU
-  ungroup() %>% 
-  mutate(redTBc = 1-prTBc) %>% # Proportional TB prevalence reduction
+outs_m <- outs %>%
+  arrange(type, run, time) %>%
+  mutate(cPCF = cPCFs + cPCFr,  # Cost of PCF (DSTB + DRTB)
+         cRxPCF = cRxPCFs + cRxPCFr,  # Cost of treatment PCF (DSTB + DRTB)
+         cRxTP = cRxTPs + cRxTPr,  # Cost of treatment ACF TP (DSTB + DRTB)
+         cRxFP = cRxFPs + cRxFPr,  # Cost of treatment ACF FP (DSTB + DRTB)
+         cCF = cPCF + cACF, # Cost of case finding
+         cRx = cRxPCF + cRxTP + cRxFP) %>% # Cost of treatment
+  group_by(run, time) %>%
+  mutate(prTBc = tTBc / tTBc[type == 'base'],  # Proportional TB prevalence to BAU
+         dfTBc = abs(tTBc - tTBc[type == 'base']),  # TB prevalence difference to BAU
+         prInc = tInc / tInc[type == 'base'],  # Proportional TB incidence to BAU
+         dfInc = abs(tInc - tInc[type == 'base']),  # TB incidence difference to BAU
+         prMor = rMor / rMor[type == 'base'],  # Proportional TB mortality to BAU
+         dfMor = abs(tMor - tMor[type == 'base']), # TB mortality difference to BAU
+         redTBc = 1 - (tTBc / tTBc[type == 'base'])) %>% # Proportional TB prevalence reduction 
+  ungroup() %>%
   group_by(type, run) %>% 
-  mutate(prFP = ifelse(tFPos == 0, 0, tFPos/(tTPos+tFPos))) %>% 
-  pivot_longer(cols = -c(time, type, run, round), names_to = "var", values_to = "values") %>% 
-  group_by(time, type, round, var) %>% 
-  summarise(val = median(values, na.rm = TRUE), 
-            lo = quantile(values, 0.025, na.rm = TRUE), 
-            hi = quantile(values, 0.975, na.rm = TRUE))
-
-outs_m <- outs %>% 
-  arrange(type, run, time) %>% 
-  group_by(run, time) %>% 
-  mutate(pIn01 = In01/In01[type == 'base'], 
-         dfIn01 = abs(In01 - In01[type == 'base']),
-         pIn02 = In02/In02[type == 'base'], 
-         dfIn02 = abs(In02 - In02[type == 'base']),
-         pIn03 = In03/In03[type == 'base'], 
-         dfIn03 = abs(In03 - In03[type == 'base']),
-         pIn05 = In05/In05[type == 'base'], 
-         dfIn05 = abs(In05 - In05[type == 'base'])) %>% 
-  pivot_longer(cols = -c(time, type, run, round), names_to = "var", values_to = "values") %>% 
-  group_by(time, type, round, var) %>% 
-  summarise(val = median(values, na.rm = TRUE), 
-            lo = quantile(values, 0.025, na.rm = TRUE), 
+  mutate(prFP = ifelse(tFPos == 0, NA, tFPos / (tTPos + tFPos))) %>% # Proportion FP
+  ungroup() %>% 
+  pivot_longer(cols = -c(time, type, run, round), names_to = "var", values_to = "values") %>%
+  group_by(time, type, round, var) %>%
+  summarise(val = median(values, na.rm = TRUE),
+            lo = quantile(values, 0.025, na.rm = TRUE),
             hi = quantile(values, 0.975, na.rm = TRUE))
   
 outs_yr <- outs %>% 
@@ -82,39 +68,21 @@ outs_yr <- outs %>%
   group_by(type, run, round) %>%
   mutate(cumTBc = cumsum(tTBc), # Cumulative TB prevalence
          cumMor = cumsum(tMor), # Cumulative TB mortality
+         cumInc = cumsum(tInc), # Cumulative TB incidence
          cumFPos = cumsum(tFPos), # Cumulative FP diagnoses
          cumTPos = cumsum(tTPos), # Cumulative TP diagnoses
          cumRx = cumsum(tFPos) + cumsum(tTPos)) %>% # Cumulative treated
   ungroup() %>% 
-  group_by(type, run) %>% 
-  mutate(cumprFP = ifelse(cumFPos == 0, 0, cumFPos/cumRx)) %>% 
-  select(time, type, run, round, cumTBc, cumMor, cumFPos, cumTPos, cumRx, cumprFP) %>% 
-  pivot_longer(cols = -c(time, type, run, round), names_to = "var", values_to = "values") %>% 
-  group_by(time, type, round, var) %>% 
-  summarise(val = median(values, na.rm = TRUE), 
-            lo = quantile(values, 0.025, na.rm = TRUE), 
-            hi = quantile(values, 0.975, na.rm = TRUE))
-
-outs_yr <- outs %>% 
-  arrange(type, run, time) %>% 
-  filter(time == floor(time)) %>% 
   group_by(run, time) %>% 
-  mutate(pIn01 = In01/In01[type == 'base'], 
-         dfIn01 = abs(In01 - In01[type == 'base']),
-         pIn02 = In02/In02[type == 'base'], 
-         dfIn02 = abs(In02 - In02[type == 'base']),
-         pIn03 = In03/In03[type == 'base'], 
-         dfIn03 = abs(In03 - In03[type == 'base']),
-         pIn05 = In05/In05[type == 'base'], 
-         dfIn05 = abs(In05 - In05[type == 'base'])) %>% 
+  mutate(dfcumInc = abs(cumInc - cumInc[type == 'base']), # TB incidence averted
+         dfcumMor = abs(cumMor - cumMor[type == 'base'])) %>% # TB mortality averted
   ungroup() %>% 
-  group_by(type, run, round) %>%
-  mutate(cumIn01 = cumsum(dfIn01),
-         cumIn02 = cumsum(dfIn02),
-         cumIn03 = cumsum(dfIn03),
-         cumIn05 = cumsum(dfIn05)) %>%
-  pivot_longer(cols = -c(time, type, run, round), names_to = "var", values_to = "values") %>% 
-  group_by(time, type, round, var) %>% 
+  group_by(type, run) %>% 
+  mutate(cumprFP = ifelse(cumFPos == 0, NA, cumFPos / cumRx)) %>% 
+  ungroup() %>%
+  select(time, type, run, round, contains('cum')) %>%
+  pivot_longer(cols = -c(time, type, run, round), names_to = "var", values_to = "values") %>%
+  group_by(time, type, round, var) %>%
   summarise(val = median(values, na.rm = TRUE), 
             lo = quantile(values, 0.025, na.rm = TRUE), 
             hi = quantile(values, 0.975, na.rm = TRUE))
@@ -122,7 +90,39 @@ outs_yr <- outs %>%
 outs <- outs_m %>% 
   rbind(outs_yr) %>% 
   arrange(factor(type, levels = c("base", "acfa", "acfb", "acfc")), round, time) 
-
 rm(outs_m, outs_yr)
 
 export(outs, here("outputs", "outs", "outs.Rdata"))
+
+# 3. Extract results ==========
+outs <- import(here("outputs", "outs", "outs.Rdata"))
+
+outini <- outs %>% 
+  filter((type == 'acfa' & round == '02' & time == 2027) |
+           (type == 'acfa' & round == '05' & time == 2030) |
+           (type == 'acfa' & round == '10' & time == 2035) |
+           (type == 'acfb' & round == '02' & time == 2027) |
+           (type == 'acfb' & round == '05' & time == 2030) |
+           (type == 'acfb' & round == '12' & time == 2037) |
+           (type == 'acfc' & round == '01' & time == 2026) |
+           (type == 'acfc' & round == '02' & time == 2027) |
+           (type == 'acfc' & round == '03' & time == 2028))
+
+filter(outini, var == 'rTBc') # TB prevalence rate
+filter(outini, var == 'redTBc') # TB prevalence rate reduction
+filter(outini, var == 'rMor') # TB mortality rate
+
+outfin <- outs %>% 
+  filter(time == 2050 & 
+           ((type == 'acfa' & round == '02') | (type == 'acfa' & round == '05') | (type == 'acfa' & round == '10') |
+            (type == 'acfb' & round == '02') | (type == 'acfb' & round == '05') | (type == 'acfb' & round == '12') | 
+            (type == 'acfc' & round == '01') | (type == 'acfc' & round == '02') | (type == 'acfc' & round == '03')))
+
+filter(outfin, var == 'dfcumInc') # TB incidence averted 
+filter(outfin, var == 'dfcumMor') # TB mortality averted
+filter(outfin, var == 'cumFPos') # Cumulative false positives
+filter(outfin, var == 'cumTPos') # Cumulative true positives
+filter(outfin, var == 'cumRx') # Cumulative treated
+filter(outfin, var == 'cumprFP') # Proportion FP treated
+
+
