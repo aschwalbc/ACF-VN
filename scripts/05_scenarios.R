2febc21ff1c66abb9347166d0411a9eb90ad779c
Alvaro Schwalb <aschwalbc@gmail.com>
2024-07-30 16:00:22 +0100

    Updating outputs and sensitivity analyses

--- scripts/05_scenarios.R
+++ scripts/05_scenarios.R
@@ -13,12 +13,11 @@ library(progress) # Displays progress bar
 
 # 1. Load data ==========
 parms <- import(here("outputs","pts","fitpts.Rdata"))[1:1000,]
-# parms <- parms %>% sample_frac(0.2)
+parms <- parms %>% sample_frac(0.2)
 WPP <- import(here("data","pop","WPP.Rdata"))
 base <- import(here("data","fit","base.Rdata"))
 DALYs <- import(here("docs","dalys","DALYs.xlsx"))
 set.seed(251195) # Set seed for randomisation
-options(scipen = 999) # Remove scientific notation
 
 # 2. Strategies ==========
 # 2.1 Baseline parameters
@@ -30,29 +29,21 @@ parms <- parms %>%
 pop_target <- 1 # Proportion of population targeted for ACF
 pop_reached <- 1 # Proportion of population participating in ACF
 
-# 2.2.1 Xpert Ultra
+# 2.2.1 Xpert Ultra ## UPDATE ##
 xpert_fp_sic <- c(lo = 0.005, hi = 0.008)
-xpert_fp_rec <- c(lo = 0.03, hi = 0.07)
-xpert_sens_min <- c(lo = 0.03, hi = 0.07)
-xpert_sens_sub <- c(lo = 0.68, hi = 0.86)
-xpert_sens_cln <- c(lo = 0.86, hi = 0.95)
-xpert_fp_tre <- c(lo = 0.03, hi = 0.30)
+xpert_fp_rec <- c(lo = 0.026, hi = 0.070)
+xpert_sens_min <- c(lo = 0.026, hi = 0.070)
+xpert_sens_sub <- c(lo = 0.676, hi = 0.856)
+xpert_sens_cln <- c(lo = 0.862, hi = 0.947)
+xpert_fp_tre <- c(lo = 0.034, hi = 0.295)
 
-# 2.2.2 Chest X-ray
+# 2.2.2 Chest X-ray ## UPDATE ##
 cxr_fp_sic <- c(lo = 0.07, hi = 0.13)
 cxr_fp_rec <- c(lo = 0.07, hi = 0.13)
 cxr_sens_min <- c(lo = 0.77, hi = 0.90)
 cxr_sens_sub <- c(lo = 0.90, hi = 0.92)
 cxr_sens_cln <- c(lo = 0.90, hi = 0.92)
 cxr_fp_tre <- c(lo = 0.48, hi = 0.52)
-
-# 2.2.3 Enhanced Xpert Ultra
-enhxpert_fp_sic <- c(lo = 0.0016, hi = 0.0029)
-enhxpert_fp_rec <- c(lo = 0.0016, hi = 0.0029)
-enhxpert_sens_min <- c(lo = 0.0016, hi = 0.0029)
-enhxpert_sens_sub <- c(lo = 0.68, hi = 0.86)
-enhxpert_sens_cln <- c(lo = 0.86, hi = 0.95)
-enhxpert_fp_tre <- c(lo = 0.01, hi = 0.08)
 
 # 2.3 ACF scenarios
 # 2.3.1 ACF Scenario A - Xpert only, Xpert+ get treatment
@@ -63,6 +54,7 @@ acfa <- data.frame(parameter = character(), lo = numeric(), hi = numeric()) %>%
   add_row(parameter = "alpha_sub", lo = xpert_sens_sub['lo'], hi = xpert_sens_sub['hi']) %>%
   add_row(parameter = "alpha_cln", lo = xpert_sens_cln['lo'], hi = xpert_sens_cln['hi']) %>% 
   add_row(parameter = "alpha_tre", lo = xpert_fp_tre['lo'], hi = xpert_fp_tre['hi']) 
+acfax <- acfa # Replicating for costing sensitivity analysis
 
 # 2.3.2 ACF Scenario B - CXR only, CXR+ get Xpert, Xpert+ get treatment
 acfb <- data.frame(parameter = character(), lo = numeric(), hi = numeric()) %>% 
@@ -72,6 +64,7 @@ acfb <- data.frame(parameter = character(), lo = numeric(), hi = numeric()) %>%
   add_row(parameter = "alpha_sub", lo = xpert_sens_sub['lo']*cxr_sens_sub['lo'], hi = xpert_sens_sub['hi']*cxr_sens_sub['hi']) %>%
   add_row(parameter = "alpha_cln", lo = xpert_sens_cln['lo']*cxr_sens_cln['lo'], hi = xpert_sens_cln['hi']*cxr_sens_cln['hi']) %>% 
   add_row(parameter = "alpha_tre", lo = xpert_fp_tre['lo']*cxr_fp_tre['lo'], hi = xpert_fp_tre['hi']*cxr_fp_tre['hi'])
+acfbx <- acfb # Replicating for costing sensitivity analysis
 
 # 2.3.3 ACF Scenario C - CXR only, CXR+ get treatment
 acfc <- data.frame(parameter = character(), lo = numeric(), hi = numeric()) %>% 
@@ -82,16 +75,7 @@ acfc <- data.frame(parameter = character(), lo = numeric(), hi = numeric()) %>%
   add_row(parameter = "alpha_cln", lo = cxr_sens_cln['lo'], hi = cxr_sens_cln['hi']) %>% 
   add_row(parameter = "alpha_tre", lo = cxr_fp_tre['lo'], hi = cxr_fp_tre['hi'])
 
-# 2.3.4 ACF Scenario D - Enhanced Xpert only, Xpert+ get treatment *Same as ACF Scenario A*
-acfd <- data.frame(parameter = character(), lo = numeric(), hi = numeric()) %>% 
-  add_row(parameter = "alpha_sic", lo = enhxpert_fp_sic['lo'], hi = enhxpert_fp_sic['hi']) %>% 
-  add_row(parameter = "alpha_rec", lo = enhxpert_fp_rec['lo'], hi = enhxpert_fp_rec['hi']) %>% 
-  add_row(parameter = "alpha_min", lo = enhxpert_sens_min['lo'], hi = enhxpert_sens_min['hi']) %>% 
-  add_row(parameter = "alpha_sub", lo = enhxpert_sens_sub['lo'], hi = enhxpert_sens_sub['hi']) %>%
-  add_row(parameter = "alpha_cln", lo = enhxpert_sens_cln['lo'], hi = enhxpert_sens_cln['hi']) %>% 
-  add_row(parameter = "alpha_tre", lo = enhxpert_fp_tre['lo'], hi = enhxpert_fp_tre['hi']) 
-
-rm(list = ls(pattern = "^(xpert|cxr|enh)"))
+rm(list = ls(pattern = "^(xpert|cxr)"))
 
 # 2.7 Other parameters
 # 2.7.1 Population parameters
@@ -113,10 +97,11 @@ scr_bau_dstb <- gamma_dist(104)
 scr_bau_drtb <- gamma_dist(500)
 
 # 2.7.2.2 Active case-finding per algorithm
-scr_acfa <- gamma_dist(8) # Normal = 8USD - Sens. analysis = 3.8USD
-scr_acfb <- gamma_dist(1.7) # Normal = 1.7USD - Sens. analysis = 1.4USD
-scr_acfc <- gamma_dist(1.2) # Normal = 1.2USD - Sens. analysis = 1.2USD
-scr_acfd <- gamma_dist(8) # Normal = 8USD - Sens. analysis = 3USD
+scr_acfa <- gamma_dist(8)
+scr_acfax <- gamma_dist(3.8)
+scr_acfb <- gamma_dist(1.7)
+scr_acfbx <- gamma_dist(1.4)
+scr_acfc <- gamma_dist(1.2)
 
 # 2.7.2.3 TB treatment
 tb_rx_dstb <- gamma_dist(81)
@@ -157,7 +142,7 @@ ode <- function(parms, base, interv = NULL, acf_times = NULL, end_time = 2050) {
   } else {
     interv_name <- deparse(substitute(interv))
     
-    if(interv_name == "acfa" | interv_name == "acfb" | interv_name == "acfd") {
+    if(interv_name %in% c('acfa', 'acfax', 'acfb', 'acfbx')) {
       prop_sputum <- 0.6
     } else if(interv_name == "acfc") {
       prop_sputum <- 1
@@ -180,12 +165,14 @@ ode <- function(parms, base, interv = NULL, acf_times = NULL, end_time = 2050) {
     
     if(interv_name == "acfa") {
       cm_screen_acf <- rgamma(n = 1, shape = scr_acfa[['shape']], scale = scr_acfa[['scale']])
+    } else if(interv_name == "acfax") {
+      cm_screen_acf <- rgamma(n = 1, shape = scr_acfax[['shape']], scale = scr_acfax[['scale']])
     } else if(interv_name == "acfb") {
       cm_screen_acf <- rgamma(n = 1, shape = scr_acfb[['shape']], scale = scr_acfb[['scale']])
+    } else if(interv_name == "acfbx") {
+      cm_screen_acf <- rgamma(n = 1, shape = scr_acfbx[['shape']], scale = scr_acfbx[['scale']])
     } else if(interv_name == "acfc") {
       cm_screen_acf <- rgamma(n = 1, shape = scr_acfc[['shape']], scale = scr_acfc[['scale']])
-    } else if(interv_name == "acfd") {
-      cm_screen_acf <- rgamma(n = 1, shape = scr_acfd[['shape']], scale = scr_acfd[['scale']])
     }
   }
   
@@ -235,38 +222,46 @@ ode <- function(parms, base, interv = NULL, acf_times = NULL, end_time = 2050) {
 
       return(list(c(
         dSUS, dSUSx, dINF, dINFx, dCLE, dCLEx, dREC, dRECx, dMIN, dMINx, dSUB, dSUBx, dCLN, dCLNx, dTXT, dTRE, dTREx),
-        rTBc    = ((SUB + CLN) / PopT * 1e5), # Infectious TB (per 100k)
-        tTBc    = (SUB + CLN), # Total infectious TB
-        rInc    = ((subcln * SUB)/ PopT * 1e5), # TB incidence (per 100k)
-        tInc    = (subcln * SUB), # Total TB incidence
-        rMor    = ((mutb * CLN) / PopT * 1e5), # Clinical TB mortality per time (per 100k)
-        tMor    = (mutb * CLN), # Clinical TB mortality per time
-        rDxs    = ((theta * CLN) / PopT * 1e5), # Notifications cTB per time in adults (per 100k)
-        tDxs    = (theta * CLN), # Total notifications cTB per time in adults
-        tInf    = (((beta / PopT) * ((kappa * SUB) + CLN)) * (SUS + CLE + (pi * REC) + (rho * TRE))), # Total infections
-        tFP     = ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_sic * (SUS + INF + CLE)) + (alpha_rec * REC) + (alpha_tre * (TRE)))), # FP diagnosed
-        tTP     = ((acf(floor(times))) * ((pop_target * pop_reached * prop_sputum * (alpha_min * MIN)) + (pop_target * pop_reached * ((alpha_sub * SUB) + (alpha_cln * CLN))))), # TP diagnosed
-        tTPM    = (acf(floor(times)) * (pop_target * pop_reached * prop_sputum * (alpha_min * MIN))), # TP minimal diagnosed
-        tTPI    = (acf(floor(times)) * (pop_target * pop_reached * ((alpha_sub * SUB) + (alpha_cln * CLN)))), # TP subclinical + clinical diagnosed
-        tScrn   = ((acf(floor(times))) * (((pop_target * pop_reached * prop_sputum) * (SUS + INF + CLE + REC + MIN + TRE)) + ((pop_target * pop_reached) * (SUB + CLN)))), # Total screened
-        cBAUs   = (((1-mdr) * (theta * CLN)) * bau_dstb), # Costs BAU DS-TB
-        cBAUr   = (((mdr) * (theta * CLN)) * bau_drtb), # Costs BAU DR-TB
-        cACF    = (((acf(floor(times))) * (((pop_target * pop_reached * prop_sputum) * (SUS + INF + CLE + REC + MIN + TRE)) + ((pop_target * pop_reached) * (SUB + CLN)))) * cm_screen_acf), # Cost ACF
-        cACFND  = (((acf(floor(times))) * ((pop_target * pop_reached * prop_sputum) * (SUS + INF + CLE + REC + TRE))) * cm_screen_acf), # Cost ACF non-disease state
-        cACFM   = (((acf(floor(times))) * ((pop_target * pop_reached * prop_sputum * MIN))) * cm_screen_acf), # Cost ACF minimal state
-        cACFI   = (((acf(floor(times))) * ((pop_target * pop_reached) * (SUB + CLN))) * cm_screen_acf), # Cost ACF infectious state
-        cRxBAUs = (((1-mdr) * (theta * CLN)) * rx_dstb), # Costs treatment BAU DS-TB
-        cRxBAUr = (((mdr) * (theta * CLN)) * rx_drtb), # Costs treatment BAU DR-TB
-        cRxFPs  = (((1-mdr) * ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_sic * (SUS + INF + CLE)) + (alpha_rec * REC) + (alpha_tre * (TRE))))) * rx_dstb), # Cost treatment false positives DS-TB
-        cRxFPr  = (((mdr) * ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_sic * (SUS + INF + CLE)) + (alpha_rec * REC) + (alpha_tre * (TRE))))) * rx_drtb), # Cost treatment false positives DR-TB
-        cRxTPs  = (((1-mdr) * ((acf(floor(times)) * ((pop_target * pop_reached * prop_sputum * (alpha_min * MIN)) + ((pop_target * pop_reached) * ((alpha_sub * SUB) + (alpha_cln * CLN))))))) * rx_dstb), # Cost treatment true positives DS-TB
-        cRxTPr  = (((mdr) * ((acf(floor(times)) * ((pop_target * pop_reached * prop_sputum * (alpha_min * MIN)) + ((pop_target * pop_reached) * ((alpha_sub * SUB) + (alpha_cln * CLN))))))) * rx_drtb), # Cost treatment true positives DR-TB
-        cRxTPMs  = (((1-mdr) * (acf(floor(times)) * (pop_target * pop_reached * prop_sputum * (alpha_min * MIN)))) * rx_dstb), # Cost treatment true positives minimal DS-TB
-        cRxTPMr  = (((mdr) * (acf(floor(times)) * (pop_target * pop_reached * prop_sputum * (alpha_min * MIN)))) * rx_drtb), # Cost treatment true positives minimal DR-TB
-        cRxTPIs  = (((1-mdr) * (acf(floor(times)) * (pop_target * pop_reached * ((alpha_sub * SUB) + (alpha_cln * CLN))))) * rx_dstb), # Cost treatment true infectious positives DS-TB
-        cRxTPIr  = (((mdr) * (acf(floor(times)) * (pop_target * pop_reached * ((alpha_sub * SUB) + (alpha_cln * CLN))))) * rx_drtb), # Cost treatment true infectious positives DR-TB
-        DALYs   = (daly(times) * (subcln * SUB)), # DALY estimates
-        ARI     = ((beta / PopT) * ((kappa * SUB) + CLN)))) # ARI
+        rTBc      = ((SUB + CLN) / PopT * 1e5), # Infectious TB (per 100k)
+        tTBc      = (SUB + CLN), # Total infectious TB
+        rInc      = ((subcln * SUB)/ PopT * 1e5), # TB incidence (per 100k)
+        tInc      = (subcln * SUB), # Total TB incidence
+        rMor      = ((mutb * CLN) / PopT * 1e5), # Clinical TB mortality per time (per 100k)
+        tMor      = (mutb * CLN), # Clinical TB mortality per time
+        tScrn     = ((acf(floor(times))) * (((pop_target * pop_reached * prop_sputum) * (SUS + INF + CLE + REC + MIN + TRE)) + ((pop_target * pop_reached) * (SUB + CLN)))), # Total number screened
+        tDiag     = ((acf(floor(times))) * (((pop_target * pop_reached * prop_sputum) * (SUS + INF + CLE + REC + MIN + TRE)) + ((pop_target * pop_reached) * (SUB + CLN)))), # Total number diagnosed
+        tSUS      = ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_sic * (SUS)))), # Total susceptible diagnosed (SUS)
+        tINF      = ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_sic * (INF)))), # Total infected diagnosed (INF)
+        tCLE      = ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_sic * (CLE)))), # Total cleared diagnosed (CLE)
+        tREC      = ((acf(floor(times)) * pop_target * pop_reached * prop_sputum) * ((alpha_rec * REC))), # Total recovered diagnosed (REC)
+        tMIN      = (acf(floor(times)) * (pop_target * pop_reached * prop_sputum * (alpha_min * MIN))), # Total minimal diagnosed (MIN)
+        tSUB      = (acf(floor(times)) * (pop_target * pop_reached * (alpha_sub * SUB))), # Total subclinical diagnosed (SUB)
+        tCLN      = (acf(floor(times)) * (pop_target * pop_reached * (alpha_cln * CLN))), # Total clinical diagnosed (CLN)
+        tTRE      = (acf(floor(times)) * (pop_target * pop_reached * (alpha_tre * (TRE)))), # Total treated diagnosed (TRE)
+        cBAUs     = (((1-mdr) * (theta * CLN)) * bau_dstb), # Costs BAU DS-TB
+        cBAUr     = (((mdr) * (theta * CLN)) * bau_drtb), # Costs BAU DR-TB
+        cACF      = (((acf(floor(times))) * (((pop_target * pop_reached * prop_sputum) * (SUS + INF + CLE + REC + MIN + TRE)) + ((pop_target * pop_reached) * (SUB + CLN)))) * cm_screen_acf), # Total cost ACF
+        cACFSUS   = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * SUS)) * cm_screen_acf), # Total cost ACF for susceptible (SUS)
+        cACFINF   = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * INF)) * cm_screen_acf), # Total cost ACF for infected (INF)
+        cACFCLE   = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * CLE)) * cm_screen_acf), # Total cost ACF for cleared (CLE)
+        cACFREC   = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * REC)) * cm_screen_acf), # Total cost ACF for recovered (REC)
+        cACFMIN   = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * MIN)) * cm_screen_acf), # Total cost ACF for minimal (MIN)
+        cACFSUB   = (((acf(floor(times))) * (pop_target * pop_reached * SUB)) * cm_screen_acf), # Total cost ACF for subclinical (SUB)
+        cACFCLN   = (((acf(floor(times))) * (pop_target * pop_reached * CLN)) * cm_screen_acf), # Total cost ACF for clinical (CLN)
+        cACFTRE   = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * TRE)) * cm_screen_acf), # Total cost ACF for treated (TRE)
+        cRxBAUs   = (((1-mdr) * (theta * CLN)) * rx_dstb), # Costs treatment BAU DS-TB
+        cRxBAUr   = (((mdr) * (theta * CLN)) * rx_drtb), # Costs treatment BAU DR-TB
+        cRxSUS    = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * SUS)) * rx_dstb), # Cost DS-TB treatment for susceptible (SUS)
+        cRxINF    = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * INF)) * rx_dstb), # Cost DS-TB treatment for infected (INF)
+        cRxCLE    = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * CLE)) * rx_dstb), # Cost DS-TB treatment for cleared (CLE)
+        cRxREC    = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * REC)) * rx_dstb), # Cost DS-TB treatment for recovered (REC)
+        cRxMIN    = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * MIN)) * rx_dstb), # Cost DS-TB treatment for minimal (MIN)
+        cRxSUBs   = ((1-mdr) * (((acf(floor(times))) * (pop_target * pop_reached * SUB)) * rx_dstb)), # Cost DS-TB treatment for subclinical (SUB)
+        cRxSUBr   = ((mdr) * (((acf(floor(times))) * (pop_target * pop_reached * SUB)) * rx_drtb)), # Cost DR-TB treatment for subclinical (SUB)
+        cRxCLNs   = ((1-mdr) * (((acf(floor(times))) * (pop_target * pop_reached * CLN)) * rx_dstb)), # Cost DS-TB treatment for clinical (CLN)
+        cRxCLNr   = ((mdr) * (((acf(floor(times))) * (pop_target * pop_reached * CLN)) * rx_drtb)), # Cost DR-TB treatment for clinical (CLN)
+        cRxTRE    = (((acf(floor(times))) * (pop_target * pop_reached * prop_sputum * TRE)) * rx_dstb), # Cost DS-TB treatment for minimal (TRE)
+        DALYs     = (daly(times) * (subcln * SUB)))) # DALY estimates
     })
   }
   
@@ -304,9 +299,10 @@ acf_year <- list(
   "12" = seq(2025, 2036, 1))
 
 outacfa <- list()
+outacfax <- list()
 outacfb <- list()
+outacfbx <- list()
 outacfc <- list()
-outacfd <- list()
 
 for (j in names(acf_year)) {
   rounds <- acf_year[[j]]
@@ -315,14 +311,17 @@ for (j in names(acf_year)) {
   if (j %in% c("03", "06", "11")) {
     outacfa[[j]] <- list()
   }
+  if (j %in% c("03", "06", "11")) {
+    outacfax[[j]] <- list()
+  }
   if (j %in% c("03", "07", "12")) {
     outacfb[[j]] <- list()
   }
+  if (j %in% c("03", "07", "12")) {
+    outacfbx[[j]] <- list()
+  }
   if (j %in% c("01", "02", "03")) {
     outacfc[[j]] <- list()
-  }
-  if (j %in% c("03", "06", "11")) {
-    outacfd[[j]] <- list()
   }
   
   pb <- progress_bar$new(format = "[:bar] :percent :eta", total = nrow(parms))
@@ -336,19 +335,24 @@ for (j in names(acf_year)) {
       outacfa[[j]][[i]] <- outacfa[[j]][[i]] %>% mutate(type = 'acfa', run = i, round = j)
     }
     
+    if (j %in% c("03", "06", "11")) {
+      outacfax[[j]][[i]] <- as.data.frame(ode(parms = curr_parms, base = curr_base, interv = acfax, acf_times = acf_year[[j]]))
+      outacfax[[j]][[i]] <- outacfax[[j]][[i]] %>% mutate(type = 'acfax', run = i, round = j)
+    }
+    
     if (j %in% c("03", "07", "12")) {
       outacfb[[j]][[i]] <- as.data.frame(ode(parms = curr_parms, base = curr_base, interv = acfb, acf_times = acf_year[[j]]))
       outacfb[[j]][[i]] <- outacfb[[j]][[i]] %>% mutate(type = 'acfb', run = i, round = j)
     }
     
+    if (j %in% c("03", "07", "12")) {
+      outacfbx[[j]][[i]] <- as.data.frame(ode(parms = curr_parms, base = curr_base, interv = acfbx, acf_times = acf_year[[j]]))
+      outacfbx[[j]][[i]] <- outacfbx[[j]][[i]] %>% mutate(type = 'acfbx', run = i, round = j)
+    }
+    
     if (j %in% c("01", "02", "03")) {
       outacfc[[j]][[i]] <- as.data.frame(ode(parms = curr_parms, base = curr_base, interv = acfc, acf_times = acf_year[[j]]))
       outacfc[[j]][[i]] <- outacfc[[j]][[i]] %>% mutate(type = 'acfc', run = i, round = j)
-    }
-    
-    if (j %in% c("03", "06", "11")) {
-      outacfd[[j]][[i]] <- as.data.frame(ode(parms = curr_parms, base = curr_base, interv = acfd, acf_times = acf_year[[j]]))
-      outacfd[[j]][[i]] <- outacfd[[j]][[i]] %>% mutate(type = 'acfd', run = i, round = j)
     }
     
     pb$tick()
@@ -360,17 +364,22 @@ for (k in 1:3) {
   assign(acfa_name, do.call(rbind, outacfa[[k]]))
   export(get(acfa_name), here("outputs", "results", paste0(acfa_name, ".Rdata")))
   
+  acfax_name <- paste0("r", names(outacfax[k]), "_outacfax_df")
+  assign(acfax_name, do.call(rbind, outacfax[[k]]))
+  export(get(acfax_name), here("outputs", "results", paste0(acfax_name, ".Rdata")))
+  
   acfb_name <- paste0("r", names(outacfb[k]), "_outacfb_df")
   assign(acfb_name, do.call(rbind, outacfb[[k]]))
   export(get(acfb_name), here("outputs", "results", paste0(acfb_name, ".Rdata")))
+  
+  acfbx_name <- paste0("r", names(outacfbx[k]), "_outacfbx_df")
+  assign(acfbx_name, do.call(rbind, outacfbx[[k]]))
+  export(get(acfbx_name), here("outputs", "results", paste0(acfbx_name, ".Rdata")))
   
   acfc_name <- paste0("r", names(outacfc[k]), "_outacfc_df")
   assign(acfc_name, do.call(rbind, outacfc[[k]]))
   export(get(acfc_name), here("outputs", "results", paste0(acfc_name, ".Rdata")))
   
-  acfd_name <- paste0("r", names(outacfd[k]), "_outacfd_df")
-  assign(acfd_name, do.call(rbind, outacfd[[k]]))
-  export(get(acfd_name), here("outputs", "results", paste0(acfd_name, ".Rdata")))
 }
 
 # 4.3 ACT3 comparison
